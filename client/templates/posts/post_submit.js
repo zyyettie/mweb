7e403a94bc66bfeaad683d9d00ecc79560ee78d0
zyyettie <zhangyuyuan1@gmail.com>
2016-04-30 15:39:40 +0800

    add post_2

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,17 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+	Meteor.call('postInsert', post, function(error, result){
+		//显示错误信息并退出
+		if(error)
+			return alert(error.reason);
+		Router.go('postPage', {_id: result._id});
+	});
+  }
+});
